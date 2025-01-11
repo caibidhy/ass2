1d4ae028749f39e2bd287497eebfd3e839c2c23e
Dong Haoyu <dhy1585326448@gmail.com>
2025-01-11 17:59:07 +0800

    Fix: Update image configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'v0.blob.com',
+          port: '',
+          pathname: '/**',
+        }
+      ],
+      unoptimized: true
+    },
+  }
+  
+  export default nextConfig
